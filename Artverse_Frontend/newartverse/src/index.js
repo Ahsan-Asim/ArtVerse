9e2c6c7ba8bab3f572cce7fbfe9b2c9fca06abf7
wajeeha kashaf <wajeehakashaf1122@gmail.com>
2025-01-15 18:53:59 +0500

    Revert "landing page"
    This reverts commit 6c751fa3febeb89a4351e9f5627be60a475cfb6d.

--- Artverse_Frontend/newartverse/src/index.js
+++ Artverse_Frontend/newartverse/src/index.js
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-/* src/styles/RightSectionSignin.css */
-import { Provider } from 'react-redux';
-import store from './Redux_store/Store.js';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
     <App />
-    </Provider>
   </React.StrictMode>
 );
 

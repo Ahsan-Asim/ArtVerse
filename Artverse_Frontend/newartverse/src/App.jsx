84d63a93d1bff5bd2fb331d133b1de331d63a1b8
Ahsan-Asim <ahsanasim252@gmail.com>
2025-01-17 10:41:29 +0500

    Signup Page completed

--- Artverse_Frontend/newartverse/src/App.jsx
+++ Artverse_Frontend/newartverse/src/App.jsx
@@ -1,16 +1,21 @@
 import './App.css';
 import { Navigation } from "./components/navigation";
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom'; // Import useLocation
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
 import Landing_Page from './pages/Landing_Page';
 import Sign_In from './pages/Sign_In';
+import Sign_Up from './pages/Sign_Up';
 
 const App = () => {
   return (
+    <GoogleOAuthProvider clientId="868206158931-8u3ftrs4ekvg4jitiu02bab01n5hj7q9.apps.googleusercontent.com">
     <div className="App">
       <Router>
         <MainContent />
       </Router>
     </div>
+    </GoogleOAuthProvider>
   );
 };
 
@@ -20,10 +25,11 @@ const MainContent = () => {
   return (
     <>
       {/* Render Navigation only on paths other than "/signin" */}
-      {location.pathname !== '/signin' && <Navigation />}
+      {location.pathname !== '/signin' && location.pathname !== '/signup' && <Navigation />}
       <Routes>
         <Route path="/" element={<Landing_Page />} />
         <Route path="/signin" element={<Sign_In />} />
+        <Route path="/signup" element={<Sign_Up />} />
       </Routes>
     </>
   );
